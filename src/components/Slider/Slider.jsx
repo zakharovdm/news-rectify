754e24a80d467553d8f7f07cbeb6b7e7a4dfb7d1
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-11-09 00:18:18 +0300

    fix slider and banners scroll

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react';
+import styles from './Slider.module.css';
+
+const Slider = ({ children, step=150 }) => {
+  const sliderRef = useRef(null);
+
+  const scrollLeft = () => {
+    sliderRef.current.scrollLeft -= 150;
+  }
+
+  const scrollRight = () => {
+    sliderRef.current.scrollLeft += 150;
+  }
+
+  return (
+    <div className={styles.slider}>
+      <button onClick={scrollLeft} className={styles.arrow}>{'<'}</button>
+      {React.cloneElement(children, { ref: sliderRef })}
+      <button onClick={scrollRight} className={styles.arrow}>{'>'}</button>
+    </div>
+  );
+};
+
+export default Slider;
