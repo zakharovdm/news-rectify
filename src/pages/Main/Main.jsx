7d44af6c50520c43022504fa7e65ae625d7ff8a3
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-11-08 23:29:38 +0300

    add latest news

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,37 +1,13 @@
-import { useDebounce } from '../../helpers/hooks/useDebounce';
 import LatestNews from '../../components/LatestNews/LatestNews';
 import styles from './Main.module.css';
-import { getNews } from '../../api/apiNews';
-import { PAGE_SIZE } from '../../constants/constants';
-import { useFetch } from '../../helpers/hooks/useFetch';
-import { useFilters } from '../../helpers/hooks/useFilters';
 import NewsByFilter from '../../components/NewsByFilter/NewsByFilter';
 
 const Main = () => {
-  const { filters, changeFilter } = useFilters({
-    page_number: 1,
-    page_size: PAGE_SIZE,
-    category: null,
-    keywords: '',
-  });
-
-  const debouncedKeywords = useDebounce(filters.keywords, 1500);
-
-  const { data, isLoading } = useFetch(getNews, {
-    ...filters,
-    keywords: debouncedKeywords,
-  });
-
   return (
     <main className={styles.main}>
-      <LatestNews isLoading={isLoading} banners={data && data.news} />
+      <LatestNews />
       
-      <NewsByFilter
-        news={data?.news}
-        isLoading={isLoading}
-        filters={filters}
-        changeFilter={changeFilter}
-      />
+      <NewsByFilter/>
     </main>
   );
 };
