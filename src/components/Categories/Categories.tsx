cb1b2e8aae525fac9ea28c5af02cc40f4e9dcb18
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-11-11 00:23:56 +0300

    migrate from js to ts

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,44 @@
+import { ForwardedRef, forwardRef } from 'react';
+import styles from './Categories.module.css';
+import { CategoriesType } from '../../interfaces';
+
+interface Props {
+  categories: CategoriesType[];
+  setSelectedCategory: (category: CategoriesType | null) => void;
+  selectedCategory: CategoriesType | null;
+}
+
+const Categories = forwardRef(
+  (
+    { categories, setSelectedCategory, selectedCategory }: Props,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    return (
+      <div ref={ref} className={styles.categories}>
+        <button
+          className={!selectedCategory ? styles.active : styles.item}
+          onClick={() => setSelectedCategory(null)}
+        >
+          All
+        </button>
+        {categories.map((category) => {
+          return (
+            <button
+              className={
+                category === selectedCategory ? styles.active : styles.item
+              }
+              onClick={() => setSelectedCategory(category)}
+              key={category}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+);
+
+Categories.displayName = 'Categories';
+
+export default Categories;
